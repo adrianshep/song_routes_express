043c728aad9c42457dc8874f09956a5150d41331
mattscilipoti <matt.scilipoti@ga.co>
2015-08-16 23:17:28 -0400

    song routes, including root redirect

--- index.js
+++ index.js
@@ -5,7 +5,43 @@ app.listen(4000, function(){
   console.log("app listening on port 4000");
 });
 
+
+// root route
+app.get('/', function (req, res){
+  res.redirect('/songs');
+});
+
 // songs#index
 app.get('/songs', function (req, res) {
   res.send('GET index of Songs');
+});
+
+// songs#new
+app.get('/songs/new', function (req, res) {
+  res.send('GET to gather info for a new Song');
+});
+
+// songs#show
+app.get('/songs/:id', function (req, res) {
+  res.send('GET to show Song:' + req.params.id);
+});
+
+// songs#create
+app.post('/songs', function (req, res) {
+  res.send('POST to create a Song');
+});
+
+// songs#edit
+app.get('/songs/:id/edit', function (req, res) {
+  res.send('GET to gather info to update Song:' + req.params.id);
+});
+
+// songs#update
+app.patch('/songs/:id', function (req, res) {
+  res.send('PATCH to update Song:' + req.params.id);
+});
+
+// songs#delete
+app.delete('/songs/:id', function (req, res) {
+  res.send('DELETE Song:' + req.params.id);
 });
